6ffeeb4a7fc542d97d4e6c9ea04ff845dd5965bd
jjruzicka <jruzicka@ucm.es>
2016-10-07 01:39:52 +0200

    Primera Pueba

---
+++ Prueba.js
@@ -0,0 +1,110 @@
+40+2;
+var point = { x: 1, y: 1 };
+point
+point.x
+var bool = true;
+bool
+var number = 1234.5;
+number
+var text = 'Hello';
+text
+point
+var object = {};
+object
+var code = function () {return 42; };
+code
+tipeof true;
+typeof true;
+typeof {};
+typeof function () { return 42; };
+var x;
+x
+point = { x:10, y: 15 };
+point
+point.label = 'origin';
+point
+var menu = [ 'attack', 'denfence', inventory'];
+var menu = [ 'attack', 'denfence', 'inventory' ];
+menu
+menu.Length;
+menu.length;
+menu.push('magic');
+menu
+menu.pop();
+menu
+menu.splice(2, 0, 'magic');
+menu
+menu.splice(2, 2, 'Ench. Inventory');
+menu
+menu.splice( 0, 0, 'wait');
+menu
+menu[0] = 'Special';
+var item = menu[10];
+menu[10] = 'Secret';
+menu
+menu.length;
+var hero = {sword: null, shild; null };
+var hero = { sword: null, shild: null );
+var hero = { sword: null, shild: null };
+hero
+hero = null;
+hero
+hero = {
+name: 'Peter',
+life: 200,
+weapon { kind: 'sword', power: 20, magicPower: 10 };
+inventory: [
+{item: null, count: 0},
+{ item: {kind 'potion', power:15 }, count: 5}
+]
+};
+
+hero
+
+hero
+hero = {
+name: 'Peter',
+weapon: {kind: 'sword', power: 20 },
+inventory: [
+{ item: null, count: 0},
+{ item: { kind: 'potion', power: 5}, count: 5}
+]
+};
+hero
+hero.inventory
+'hola' === 'hola';
+12 === 12;
+12 === 10+2;
+12 !== 12;
+{} === {};
+({} === {});
+[1, 2, 3] ===  [1, 2, 3];
+var obj = {};
+var obj2 = obj;
+obj2 === {};
+obj
+obj2
+obj === obj2;
+var enemy = {
+Direccion: 'rigth',
+position: { x: 10, y: 10 },
+score: 40
+, };
+enemy
+enemy.label = moveLeft: function () { console.log('Izquierda'); };
+var label = function () { console.log('Izquierda'); };
+var moverIzq = function () { console.log('Izquierda'); };
+enemy
+enemy.moverIzq
+enemy
+var moverizq = enemy.moverizq
+enemy.moverizq = function () { console.log('Izquierda'); };
+enemy
+var moverder = enemy.moverder;
+enemy.moverder = function () { console.log('Derecha'); };
+enemy
+enemy.moverder = function () {this.position.x += 2; };
+enemy.moverizq = function () {this.position.x -= 2; };
+enemy
+enemy.moverizq();
+enemy
